a5a64d80a8553c408d8b5689c2adfe9091a6d68c
Thales Reis <thales.reisfurtado@gmail.com>
2023-10-27 15:00:51 -0300

    Update na estrutura do código e na linha 20 e 21

--- Classificador_nivel_heroi.js
+++ Classificador_nivel_heroi.js
@@ -1,27 +1,48 @@
 const nomeDoHeroi = "Ralph"; 
 const xpDoHeroi = 10001; 
-let nivelDoHeroi;
 
-if (xpDoHeroi < 1000) {
-  nivelDoHeroi = "Ferro";
-} else if (xpDoHeroi >= 1001 && xpDoHeroi <= 2000) {
-  nivelDoHeroi = "Bronze";
-} else if (xpDoHeroi >= 2001 && xpDoHeroi <= 5000) {
-  nivelDoHeroi = "Prata";
-} else if (xpDoHeroi >= 6001 && xpDoHeroi <= 7000) {
-  nivelDoHeroi = "Ouro";
-} else if (xpDoHeroi >= 7001 && xpDoHeroi <= 8000) {
-  nivelDoHeroi = "Platina";
-} else if (xpDoHeroi >= 8001 && xpDoHeroi <= 9000) {
-  nivelDoHeroi = "Ascendente";
-} else if (xpDoHeroi >= 9001 && xpDoHeroi <= 10000) {
-  nivelDoHeroi = "Imortal";
-} else {
-  nivelDoHeroi = "Radiante";
-}
+switch (true) {
+    case xpDoHeroi < 1000:
+        if (xpDoHeroi < 1000) {
+            nivelDoHeroi = "Ferro";           
+        }break;
 
-console.log("O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}.");
+    case xpDoHeroi >= 1001 && xpDoHeroi <= 2000:
+        if (xpDoHeroi  >= 1001 && xpDoHeroi <= 2000) {
+            nivelDoHeroi ="Bronze"
+        }break
 
+    case xpDoHeroi >= 2001 && xpDoHeroi <= 5000:
+        if (xpDoHeroi  >= 2001 && xpDoHeroi <= 5000) {
+            nivelDoHeroi ="Prata"
+        }break
+
+    case xpDoHeroi >= 5001 && xpDoHeroi <= 7000:
+        if (xpDoHeroi  >= 5001 && xpDoHeroi <= 7000) {
+            nivelDoHeroi ="Ouro"
+        }break
+
+    case xpDoHeroi >= 7001 && xpDoHeroi <= 8000:
+        if (xpDoHeroi  >= 7001 && xpDoHeroi <= 8000) {
+            nivelDoHeroi ="Platina"
+        }break
+
+    case xpDoHeroi >= 8001 && xpDoHeroi <= 9000:
+        if (xpDoHeroi  >= 8001 && xpDoHeroi <= 9000) {
+            nivelDoHeroi ="Ascendente"
+        }break
+
+    case xpDoHeroi >= 9001 && xpDoHeroi <= 10000:
+        if (xpDoHeroi  >= 9001 && xpDoHeroi <= 10000) {
+            nivelDoHeroi ="Imortal"
+        }break
+    
+    default:
+        nivelDoHeroi = "Radiante"
+        break
+    }
+    
+    console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}.`);
 
 
   
